8af33f9db8bea4aec2c0e4831e675cbd4535e926
Pratham Mittal <prathamm4402@gmail.com>
2025-03-05 09:55:15 +0530

    Added Navbar component and updated README for Milestone 15

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,21 @@
- 
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import {LoginPage,SignupPage,Home,CreateProduct, MyProducts} from "./Routes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage, SignupPage, Home, CreateProduct, MyProducts } from "./Routes";
+import Navbar from "./components/Navbar";  // Import Navbar
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar /> {/* Add Navbar here */}
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/create-product" element={<CreateProduct />} />
         <Route path="/myproducts" element={<MyProducts />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Frontend/src/components/auth/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 text-white p-4 flex justify-between">
+      <h2 className="text-xl font-bold">MyShop</h2>
+      <ul className="flex space-x-4">
+        <li><Link to="/" className="hover:text-gray-400">Home</Link></li>
+        <li><Link to="/myproducts" className="hover:text-gray-400">My Products</Link></li>
+        <li><Link to="/create-product" className="hover:text-gray-400">Add Product</Link></li>
+        <li><Link to="/login" className="hover:text-gray-400">Login</Link></li>
+        <li><Link to="/signup" className="hover:text-gray-400">Sign Up</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
