8909c11e30e774ee69c1001db5a892b97d50b424
Pratham Mittal <prathamm4402@gmail.com>
2025-03-19 10:28:48 +0530

    Completed Milestone 26: Added backend endpoint to get user orders

--- Backend/routes/order.js
+++ Backend/routes/order.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-const { placeOrder } = require("../controllers/order");
+const { placeOrder, getUserOrders } = require("../controllers/order");
 
 router.post("/place", auth, placeOrder);
+router.post("/orders", auth, getUserOrders); 
 
 module.exports = router;
